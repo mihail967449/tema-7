ee928bbaa49afb0681de863558f65bb2b441b2ed
mihail967449 <127037516+mihail967449@users.noreply.github.com>
2023-03-05 17:13:09 +0200

    goto

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //1 задание 
+            //Console.OutputEncoding = Encoding.UTF8;
+            //Console.WriteLine("Введите слово: ");
+            //string privet = Console.ReadLine();
+            //Console.WriteLine("Сколько раз вывести слово? ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.WriteLine(privet);
+            //}
+
+
+            //2.задание 
+            //for (int x = 10; x <= 99; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+
+            //3.задание 
+            //for (int x = 10; x <= 99; x++)
+            //{
+            //    if (x % 11 == 0)
+            //        Console.WriteLine(x);
+            //}
+
+            //4 задание
+            //Console.OutputEncoding = Encoding.UTF8;
+            //int count = 0;
+            //for (int i = 10; i <= 99; i++)
+            //{ 
+            //    if (i % 16 == 0)
+            //    { 
+            //        Console.WriteLine(i); 
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine("Количество двухзначных чисел, кратных 16: " + count);
+
+            //5 задание
+            //Console.OutputEncoding = Encoding.UTF8;
+            //int k = 0;
+            //int s = 0;
+
+            //for (int i = 10; i <= 99; i++)
+            //{
+            //    if (i % 25 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //        k++;
+            //        s += i;
+            //    }
+            //}
+
+            //Console.WriteLine($"Количество: {k}");
+            //Console.WriteLine($"Сумма: {s}");
+
+
+
+            //задание 6
+            //string s = "#######. #######. #######. #######";
+
+            //string[] subs = s.Split(' ', '.');
+
+            //foreach (var sub in subs)
+            //{
+            //    Console.WriteLine($"{sub}");
+            //}
+
+            //задание 7
+            //foreach (char i in "###############   ###############   ###############   ###############   ###############   ###############   ###############   ###############")
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
